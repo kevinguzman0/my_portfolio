ec8eb9bcedba73d80bd450d157ee97094c083078
kevinguzman0 <guzmankevin90@gmail.com>
2021-12-07 10:36:44 -0500

    fotos actualizadas 2

--- src/components/Content/Home.js
+++ src/components/Content/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
         <div id="content-home">
             <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-                    <CarouselItem active="active" src={carousel1} >
+                    <CarouselItem active="active" src={carousel2} >
                         <h1>Hi! <br />I'm Kevin</h1>
                     </CarouselItem>
-                    <CarouselItem active="" src={carousel2} >
+                    <CarouselItem active="" src={carousel1} >
                         <h1>I am <br />Developer</h1>
                     </CarouselItem>
                 </div>

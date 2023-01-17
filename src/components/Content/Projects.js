ac290b787b47ec3edfb2f0867a389aabe2806c57
kevinguzman0 <guzmankevin90@gmail.com>
2023-01-17 14:40:45 -0500

    ajustes app 17/01/2023

--- src/components/Content/Projects.js
+++ src/components/Content/Projects.js
@@ -1,67 +1,72 @@
-import InformationBasic from './InformationBasic'
-import BoxProject from './BoxProject'
+import InformationBasic from "./InformationBasic";
+import BoxProject from "./BoxProject";
 
 import sendme from "../../img/sendme.png";
 import team from "../../img/team.png";
+import people from "../../img/people.png";
 import food from "../../img/food.jpg";
-import active from "../../img/active.png"
+import finanzly from "../../img/finanzly.png";
 
 const Projects = () => {
+  const projectData = [
+    {
+      image: sendme,
+      link: "https://github.com/kevinguzman0/send_me_sockets",
+      title: "SendME",
+      description1: "Sending information, client server through SocketIO",
+      description2: "Developed with NodeJS, Express, MongoDB and React",
+    },
+    {
+      image: team,
+      link: "https://github.com/kevinguzman0/team_manager",
+      title: "Teamanager",
+      description1: "Organize your team",
+      description2: "Developed in React with Redux",
+    },
+    {
+      image: people,
+      link: "https://github.com/kevinguzman0/project_typescript_vite",
+      title: "FavoritePeople",
+      description1: "Filter people and save them as favorites",
+      description2:
+        "Developed in React with Vite, TypeScript, React mui, Datagrid and Redux",
+    },
+    {
+      image: finanzly,
+      link: "https://github.com/kevinguzman0/finanzly",
+      title: "Finanzle",
+      description1: "Save your finances",
+      description2:
+        "Developed in React, redux and testing with Jest and React Testing Library",
+    },
+    {
+      image: food,
+      link: "https://github.com/kevinguzman0/order_food",
+      title: "FoodOrder",
+      description1: "Enter and ask your order",
+      description2:
+        "Developed in React Native and connected to API NodeJS with Firebase",
+    },
+  ];
 
-    const projectData = [
-        {
-            image: sendme,
-            link: 'https://sendme-project.herokuapp.com/',
-            title: 'SendME',
-            description1: 'Sending information, client server through SocketIO',
-            description2: 'Developed in NodeJs and React with MongoDB'
-        },
-        {
-            image: team,
-            link: 'https://kevinteamanager.herokuapp.com/',
-            title: 'Teamanager',
-            description1: 'Organize your team',
-            description2: 'Developed in React with Redux'
-        },
-        {
-            image: food,
-            link: 'https://github.com/kevinguzman0/order_food',
-            title: 'FoodOrder',
-            description1: 'Enter and ask your order',
-            description2: 'Developed in React Native and connected to API NodeJS with Firebase'
-        },
-        {
-            image: active,
-            link: 'https://play.google.com/store/apps/details?id=com.denspalm.activemarket&pli=1',
-            title: 'ActiveMarket',
-            description1: 'Connect companies with Influencers',
-            description2: 'Developed in React Native with Expo'
-        }
-    ]
+  return (
+    <div>
+      <InformationBasic span="my work" h2="my projects" />
 
-    return (
-        <div>
-            <InformationBasic
-                span='my work'
-                h2='my projects'
-            />
+      <div className="row wow animate__animated animate__fadeInUp animate__slow">
+        {projectData.map((e, i) => (
+          <BoxProject
+            key={i}
+            image={e.image}
+            link={e.link}
+            title={e.title}
+            desc={e.description1}
+            desc2={e.description2}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            <div className="row wow animate__animated animate__fadeInUp animate__slow">
-                {projectData.map((e, i) => (
-                    <BoxProject
-                        key={i}
-                        image={e.image}
-                        link={e.link}
-                        title={e.title}
-                        desc={e.description1}
-                        desc2={e.description2}
-                    />
-                ))}
-
-
-            </div>
-        </div>
-    )
-}
-
-export default Projects
+export default Projects;
--- src/components/Content/Skills.js
+++ src/components/Content/Skills.js
@@ -1,120 +1,120 @@
-import InformationBasic from './InformationBasic'
-import BarraSkills from './BarraSkills'
+import InformationBasic from "./InformationBasic";
+import BarraSkills from "./BarraSkills";
 
 const Skills = () => {
+  const basicSkillsData = [
+    {
+      name: "HTML5",
+      porcent: "80%",
+      color: "#FF7D00",
+    },
+    {
+      name: "CSS3",
+      porcent: "75%",
+      color: "#2C98F0",
+    },
+    {
+      name: "JavaScript",
+      porcent: "70%",
+      color: "#FFCB00",
+    },
+    {
+      name: "TypeScript",
+      porcent: "55%",
+      color: "#2f74c0",
+    },
+    {
+      name: "NodeJS",
+      porcent: "70%",
+      color: "#90C53F",
+    },
+    {
+      name: "React",
+      porcent: "80%",
+      color: "#4AD5FF",
+    },
+    {
+      name: "React Native",
+      porcent: "80%",
+      color: "#4AD5FF",
+    },
+    {
+      name: "Jest",
+      porcent: "55%",
+      color: "#813754",
+    },
+  ];
 
-    const basicSkillsData = [
-        {
-            name: 'HTML5',
-            porcent: '80%',
-            color: '#FF7D00'
-        },
-        {
-            name: 'CSS3',
-            porcent: '75%',
-            color: '#2C98F0'
-        },
-        {
-            name: 'JavaScript',
-            porcent: '70%',
-            color: '#FFCB00'
-        },
-        {
-            name: 'NodeJS',
-            porcent: '70%',
-            color: '#90C53F'
-        },
-        {
-            name: 'React',
-            porcent: '80%',
-            color: '#4AD5FF'
-        },
-        {
-            name: 'React Native',
-            porcent: '80%',
-            color: '#4AD5FF'
-        },
-    ]
+  const databasesSkillsData = [
+    {
+      name: "MySQL",
+      porcent: "70%",
+      color: "#FF7D00",
+    },
+    {
+      name: "PostgreSQL",
+      porcent: "60%",
+      color: "#2C98F0",
+    },
+    {
+      name: "MongoDB",
+      porcent: "60%",
+      color: "#90C53F",
+    },
+  ];
 
-    const databasesSkillsData = [
-        {
-            name: 'MySQL',
-            porcent: '70%',
-            color: '#FF7D00'
-        },
-        {
-            name: 'PostgreSQL',
-            porcent: '60%',
-            color: '#2C98F0'
-        },
-        {
-            name: 'MongoDB',
-            porcent: '60%',
-            color: '#90C53F'
-        }
-    ]
+  const extraSkillsData = [
+    {
+      name: "Linux",
+      porcent: "80%",
+      color: "#FFCB00",
+    },
+    {
+      name: "Git",
+      porcent: "80%",
+      color: "#E84D31",
+    },
+  ];
 
-    const extraSkillsData = [
-        {
-            name: 'Linux',
-            porcent: '80%',
-            color: '#FFCB00'
-        },
-        {
-            name: 'Bootstrap',
-            porcent: '80%',
-            color: '#5A3A7B'
-        },
-        {
-            name: 'Git',
-            porcent: '80%',
-            color: '#E84D31'
-        }
-    ]
+  return (
+    <div>
+      <InformationBasic
+        span="My specialty"
+        h2="My skills"
+        p="In the projects carried out I developed skills in HTML, CSS, JavaScript, TypeScript, React, React Native, NodeJS, Jest, Git, MySQL, MongoDB, PostgreSQL. I want to continue deepening my knowledge, learn other technologies and abilities."
+      ></InformationBasic>
+      <div className="row wow animate__animated animate__fadeInRight animate__slow">
+        {basicSkillsData.map((e, i) => (
+          <BarraSkills
+            key={i}
+            skill={e.name}
+            porcentaje={e.porcent}
+            colorbar={e.color}
+          />
+        ))}
 
-    return (
-        <div>
-            <InformationBasic
-                span='My specialty'
-                h2='My skills'
-                p="In the projects carried out I developed skills in HTML, CSS and JavaScript, Git, Bootstrap, MySQL, MongoDB, PostgreSQL, React, React-Native, NodeJS. I want to continue deepening my knowledge, learn other technologies and abilities."
-            >
-            </InformationBasic>
-            <div className="row wow animate__animated animate__fadeInRight animate__slow">
+        <span className="title-about mt-3">Databases</span>
+        {databasesSkillsData.map((e, i) => (
+          <BarraSkills
+            key={i}
+            skill={e.name}
+            porcentaje={e.porcent}
+            colorbar={e.color}
+          />
+        ))}
 
-                {basicSkillsData.map((e, i) => (
-                    <BarraSkills
-                        key={i}
-                        skill={e.name}
-                        porcentaje={e.porcent}
-                        colorbar={e.color}
-                    />
-                ))}
-
-                <span className="title-about mt-3">Databases</span>
-                {databasesSkillsData.map((e, i) => (
-                    <BarraSkills
-                        key={i}
-                        skill={e.name}
-                        porcentaje={e.porcent}
-                        colorbar={e.color}
-                    />
-                ))}
-
-                <span className="title-about mt-3">Extra</span>
-                {extraSkillsData.map((e, i) => (
-                    <BarraSkills
-                        key={i}
-                        skill={e.name}
-                        porcentaje={e.porcent}
-                        colorbar={e.color}
-                    />
-                ))}
-
-            </div>
-            <br />
-
-        </div>
-    )
-}
-export default Skills
+        <span className="title-about mt-3">Extra</span>
+        {extraSkillsData.map((e, i) => (
+          <BarraSkills
+            key={i}
+            skill={e.name}
+            porcentaje={e.porcent}
+            colorbar={e.color}
+          />
+        ))}
+      </div>
+      <br />
+    </div>
+  );
+};
+export default Skills;
